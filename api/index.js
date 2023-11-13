46855c0d81d02968b3d8ab03cc3f6f941b205a49
Abdi Tefera <abditefera11@gmail.com>
2023-11-14 01:34:00 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 
 const app = express();
 
-const MONGODB_API = "mongodb+srv://abdi:abditefera@cluster0.3cenfxy.mongodb.net/realstate";
 
-
+mongoose.connect(process.env.MONGODB_API)
+        .then(() => console.log("MongoDB Connected..."))
+        .catch((error) => console.log(`MongoDB not Connected...${error}`))
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
